020ac30925775c7f06faa5655610466d0bda65d1
Greg Duncan <Greg Duncan>
2017-05-21 16:52:16 +1000

    app objects

--- js/myApp.js
+++ js/myApp.js
@@ -1,1 +1,24 @@
-console.log('myApp')
+console.log('myApp');
+
+// you just signed up 
+const appData = {
+    questions: ['test question'],
+
+    
+}
+
+const handlers = {
+
+}
+
+const view = {
+    makeButton: function(yesorno) {
+        const myButton = document.createElement('button');
+        myButton.context = yesorno;
+        myButton.className = btn;
+        return myButton;
+    },
+    addButton: function(yesorno){
+        this.makeButton(yesorno);
+    }
+}
